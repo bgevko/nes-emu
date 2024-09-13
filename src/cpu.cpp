d8cdad8470f21d0e8f1cdfba0031ca7af2bdc2fb
Bogdan Gevko <bgevko@gmail.com>
2024-09-13 16:29:20 +0300

    Initialized C++ project

---
+++ src/cpu.cpp
@@ -0,0 +1,5 @@
+// cpu.cpp
+
+#include <iostream>
+
+void test() { std::cout << "Hello cpu!" << std::endl; }
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// cpu.cpp
+
+#include "cpu.h"
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  test();
+  return 0;
+}
